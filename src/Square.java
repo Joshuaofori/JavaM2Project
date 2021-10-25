feacb36b36083686d5942d6278afd0d5f698c641
Joshuaofori <oforijoshua37@gmail.com>
2021-10-25 14:04:41 +0200

    boilerplates version 0.8

---
+++ src/Square.java
@@ -0,0 +1,36 @@
+public class Square {
+    private int verticalIndex;
+    private int horizontalIndex;
+    Nation nation;
+
+    public Square(){}
+    public Square(int verticalIndex, int horizontalIndex,Nation nation){
+        this.horizontalIndex=horizontalIndex;
+        this.verticalIndex=verticalIndex;
+        this.nation=nation;
+    }
+
+    public int getVerticalIndex() {
+        return verticalIndex;
+    }
+
+    public void setVerticalIndex(int verticalIndex) {
+        this.verticalIndex = verticalIndex;
+    }
+
+    public int getHorizontalIndex() {
+        return horizontalIndex;
+    }
+
+    public void setHorizontalIndex(int horizontalIndex) {
+        this.horizontalIndex = horizontalIndex;
+    }
+
+    public Nation getNation() {
+        return nation;
+    }
+
+    public void setNation(Nation nation) {
+        this.nation = nation;
+    }
+}
