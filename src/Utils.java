0f8967cf911916b7fd5198bd1ea3cf3267dc482b
Jelena Milic <jelenamilic1999.jm@gmail.com>
2021-11-14 23:30:58 +0100

    Implemented random methods in Utils

--- src/Utils.java
+++ src/Utils.java
@@ -1,2 +1,30 @@
+import java.util.Random;
+
 public class Utils {
+
+    public static Direction getRandomDirection() {
+        Random random = new Random();
+        switch (random.nextInt(8)) {
+            case 0: return Direction.NORTH;
+            case 1: return Direction.NORTHEAST;
+            case 2: return Direction.EAST;
+            case 3: return Direction.SOUTHEAST;
+            case 4: return Direction.SOUTH;
+            case 5: return Direction.SOUTHWEST;
+            case 6: return Direction.WEST;
+            case 7: return Direction.NORTHWEST;
+        }
+
+        return null;
+    }
+
+    public static int getRandomNumberOfSquaresToMove() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
+
+    public static int getRandomNumberOfMessages() {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
 }
