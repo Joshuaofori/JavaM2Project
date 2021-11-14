6daab14ab739a8c66246e44ecb54ced679876375
Joshuaofori <oforijoshua37@gmail.com>
2021-11-14 13:28:52 +0100

    beings generated on map final version

--- src/Map.java
+++ src/Map.java
@@ -2,6 +2,7 @@ import java.util.Random;
 public class Map {
     private int m,n;
     Nation allNations[]={Nation.REBELS,Nation.JEDI, Nation.GALACTIC_EMPIRE,Nation.STORMTROOPERSS};
+
    private Square[][] squares;
 
    Random random;
@@ -16,22 +17,26 @@ public class Map {
     squares= new Square[m][n];
   }
   private void generateBeings(){
+      
         squares[0][0]= new Square(0,0,new SimulationObject(Nation.JEDI),m,n);
         squares[m - 1][0]= new Square(m-1,0,new SimulationObject(Nation.REBELS),m,n);
         squares[0][n-1]=new Square(0,n-1,new SimulationObject(Nation.GALACTIC_EMPIRE),m,n);
         squares[m-1][n-1]= new Square(m-1,n-1,new SimulationObject(Nation.STORMTROOPERSS),m,n);
         int mPositon,nPosition;
+
         for(Nation aNation: allNations){
         for(int i=0;i<4;i++){
-            mPositon=random.nextInt(8);
-            nPosition=random.nextInt(14);
-        if(squares[mPositon][nPosition]==null){
-         squares[mPositon][nPosition]= new Square(mPositon,nPosition,new SimulationObject(aNation),m,n);
-        }
-        else{
-            nPosition=random.nextInt(14);
-            squares[mPositon][nPosition]= new Square(mPositon,nPosition, new SimulationObject(aNation),m,n);
-        }
+
+            int r = random.nextInt(56)/8;
+            int c = random.nextInt(182)%14;
+
+           r+= (r<3&&c<3&&aNation!=Nation.JEDI)?3:0;
+           r+= (r<3&&c>10&&aNation!=Nation.GALACTIC_EMPIRE)?3:0;
+           r-= (r>4&&c<3&&aNation!=Nation.REBELS)?3:0;
+           r-= (r>4&&c>10&&aNation!=Nation.STORMTROOPERSS)?3:0;
+
+            squares[r][c]= new Square(r,c,new SimulationObject(aNation),m,n);
+//
 
         }}
   }
@@ -61,4 +66,5 @@ public class Map {
             System.out.println();
         }
  }
+
 }
